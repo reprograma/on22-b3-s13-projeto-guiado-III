3a1a812817892ab795a936a613b8fdad8471344d
nearlyerika <72031082+nearlyerika@users.noreply.github.com>
2023-06-23 16:32:59 -0300

    refactoring almost done

--- exercicios/gameStore/src/controllers/gamesController.js
+++ exercicios/gameStore/src/controllers/gamesController.js
@@ -1,18 +1,9 @@
-const GamesModel = require("../models/gamesModel");
-const ConsolesModel = require("../models/consolesModel");
+const GamesModel = require('../models/gamesModel');
+const ConsolesModel = require('../models/consolesModel');
 
 const addNewGame = async (req, res) => {
   try {
-    const {
-      consoleId,
-      name,
-      developer,
-      releaseDate,
-      genre,
-      mode,
-      available,
-      description,
-    } = req.body;
+    const { consoleId, ...gameData } = req.body;
 
     if (!consoleId) {
       return res
@@ -20,26 +11,25 @@ const addNewGame = async (req, res) => {
         .json({ message: 'Required: Enter the Console id.' });
     }
 
-    const findConsole = await ConsolesModel.findById(consoleId);
+    const foundConsole = await ConsolesModel.findById(consoleId);
 
-    if (!findConsole) {
-      return res.status(404).json({ message: 'Console not found' });
+    if (!foundConsole) {
+      return res
+        .status(404)
+        .json({ message: 'Console not found' });
     }
 
     const newGame = new GamesModel({
       console: consoleId,
-      name,
-      developer,
-      releaseDate,
-      genre,
-      mode,
-      available,
-      description,
+      ...gameData,
     });
+
     const savedGame = await newGame.save();
-    res
-      .status(200)
-      .json({ message: 'New game added successfully!', savedGame });
+
+    res.status(201).json({
+      message: 'Your new game was saved successfully.',
+      savedGame,
+    });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: error.message });
@@ -50,18 +40,21 @@ const findAllGames = async (req, res) => {
   try {
     const allGames = await GamesModel.find().populate('console');
     res.status(200).json(allGames);
-  } catch {
+  } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
 
 const findGameById = async (req, res) => {
   try {
-    const findGame = await GamesModel.findById(
-      req.params.id
-    ).populate('console');
-    if (findGame == null) {
-      res.status(404).json({ message: 'Game not available' });
+    const { id } = req.params;
+    const findGame = await GamesModel.findById(id).populate(
+      'console'
+    );
+    if (!findGame) {
+      return res
+        .status(404)
+        .json({ message: 'Game not available' });
     }
     res.status(200).json(findGame);
   } catch (error) {
@@ -69,46 +62,21 @@ const findGameById = async (req, res) => {
   }
 };
 
-
 const updateGame = async (req, res) => {
   try {
     const { id } = req.params;
-    const {
-      consoleId,
-      name,
-      developer,
-      releaseDate,
-      genre,
-      mode,
-      available,
-      description,
-    } = req.body;
-    const findGame = await GamesModel.findById(id);
-    if (findGame == null) {
-      res.status(404).json({ message: 'Game not found' });
-    }
+    const updatedGame = await GamesModel.findByIdAndUpdate(
+      id,
+      req.body,
+      { new: true }
+    ).populate('console');
 
-    if (consoleId) {
-      const findConsole = await ConsolesModel.findById(consoleId);
-
-      if (findConsole == null) {
-        return res.status(404).json({ message: 'Console not found' });
-      }
-    }
-    findGame.name = name || findGame.name;
-    findGame.developer = developer || findGame.developer;
-    findGame.releaseDate = releaseDate || findGame.releaseDate;
-    findGame.genre = genre || findGame.genre;
-    findGame.mode = mode || findGame.mode;
-    findGame.available = available || findGame.available;
-    findGame.description = description || findGame.description;
-    findGame.console = consoleId || findGame.console;
-
-    const savedGame = await findGame.save();
-    res
-      .status(200)
-      .json({ message: 'Game successfully updated', savedGame });
+    res.status(200).json({
+      message: 'Game updated and saved.',
+      updatedGame,
+    });
   } catch (error) {
+    console.error(error);
     res.status(500).json({ message: error.message });
   }
 };
@@ -116,27 +84,25 @@ const updateGame = async (req, res) => {
 const deleteGame = async (req, res) => {
   try {
     const { id } = req.params;
-    const findGames = await GamesModel.findByIdAndDelete(id);
+    const findGame = await GamesModel.findByIdAndDelete(id);
 
-    if (findGames == null) {
+    if (findGame == null) {
       return res
         .status(404)
-        .json({ message: `Game with id ${id} not found` });
+        .json({message: `Game with id ${id} not found`});
     }
-    res
-      .status(200)
-      .json({
-        message: `Game with id ${id} was successfully deleted`,
-      });
+    res.status(200).json({
+      message: `Game with id ${id} was successfully deleted`,
+    });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
 
 module.exports = {
+  addNewGame,
   findAllGames,
   findGameById,
-  addNewGame,
   updateGame,
   deleteGame,
 };
