0181981b6f41830cb669f0515a55e480d23ef318
ModoEconomico <patriciaalves1515@gmail.com>
2023-08-02 00:10:50 -0300

    finalizado

--- exercicios/gameStore/src/models/gamesModel.js
+++ exercicios/gameStore/src/models/gamesModel.js
@@ -1,51 +1,59 @@
 const mongoose = require("mongoose");
+const gameSchema = mongoose.Schema(
+    {
+        _id: {
+            type: mongoose.Schema.Types.ObjectId,
+            default: () => new mongoose.Types.ObjectId(),
+          },
 
-const GameSchema = mongoose.Schema(
-  {
-    _id: {
-      type: mongoose.Schema.Types.ObjectId,
-      default: () => new mongoose.Types.ObjectId(),
-    },
-    name: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    developer: {
-      type: String,
-      required: true,
-    },
-    releaseDate: {
-      type: Number,
-      required: true,
-    },
-    genre: {
-      type: [String],
-      required: true,
-    },
-    mode: {
-      type: [String],
-      required: true,
-    },
-    available: {
-      type: Boolean,
-      required: true,
-    },
-    description: {
-        type: String,
-        minLength: 0,
-        maxLength: 500,
-        deafult: "Não informado",
-      },
-    console: {
-      type: mongoose.Schema.Types.ObjectId,
-      required: true,
-      ref: "Console",
-    },
-  },
-  { timestamp: true }
-);
+          name: {
+            type: String,
+            required: true,
+            unique: true,
+          },
 
-const Model = mongoose.model("Game", GameSchema);
+          developer: {
+            type: String,
+            required: true,
+          },
 
-module.exports = Model;
+          releaseDate: {
+            type: Number,
+            required: true,
+          },
+
+          genre: {
+            type: [String],
+            required: true,
+          },
+
+          mode: {
+            type: [String],
+            required: true,
+          },
+
+          available: {
+            type: Boolean,
+            required: true,
+          },
+
+          description: {
+            type: String,
+            minLenght: 0, // minimo de caractéres
+            maxLenght: 500,
+            default: "Não Informado!"
+          },
+
+          console: {
+            type: mongoose.Schema.Types.ObjectId, // objectId pode ser substituido por .name mas na lógica de programação precisa usar o ToLowerCase
+            required: true,
+            ref: "Console", //fazendo a referencia dele o console e games para usar no .populate
+          }
+        },
+        { timestamp: true }
+      );
+      
+      const Model = mongoose.model("Game", gameSchema);
+      
+      module.exports = Model;
+    
